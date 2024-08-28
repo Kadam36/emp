7384d4dcf75fd74350eadb0d94b2ac174425593a
Kadam36 <swapnilkadam0326@gmail.com>
2024-08-28 18:35:46 +0530

    settings

--- Emp/settings.py
+++ Emp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=3u8xazk600z#jn_%1te16(=t$ecr^70x)auiyda5&cat5t8@0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 
