aeca607046bd60eada5e0e3907929e4d10e6ac12
Kadam36 <swapnilkadam0326@gmail.com>
2024-08-28 17:55:35 +0530

    SETTING

--- Emp/settings.py
+++ Emp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import dj_database_url 
 from pathlib import Path
-
+from django.core.wsgi import get_wsgi_application
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -72,6 +72,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Emp.wsgi.application'
+
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Emp.settings')
+
+application = get_wsgi_application()
 
 
 # Database
